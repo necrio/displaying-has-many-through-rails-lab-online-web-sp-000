2e6446e36b197a63c6e8a0dea5ef95a52412009b
Keith Isham <kisham8899@gmail.com>
2020-01-29 12:37:46 +0000

    Automatically backed up by Learn

--- db/migrate/20200129123354_create_appointments.rb
+++ db/migrate/20200129123354_create_appointments.rb
@@ -1,8 +1,13 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
+      t.integer :doctor_id
+      t.integer :patient_id
+      t.datetime :appointment_datetime
 
-      t.timestamps
+     t.timestamps null: false
+
+
     end
   end
 end
